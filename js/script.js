3adfb38112a91ed2c9071360ab8a6b24f98e89f1
kazuno28 <kazuki.soccer1013.no28@gmail.com>
2021-11-26 17:48:30 +0900

    イベント設定

--- js/script.js
+++ js/script.js
@@ -1,23 +1,45 @@
-$(function(){
-  $('.menu-trigger').on('click', function(event){
-    $(this).toggleClass('active');
-    $('#sp-menu').fadeToggle();
-    event.preventDefault();
-  });
+$('#tab-contents .tab[id != "tab1"]').hide();
+
+$('#tab-menu a').on('click', function(event) {
+  $("#tab-contents .tab").hide();
+  $("#tab-menu .active").removeClass("active");
+  $(this).addClass("active");
+  $($(this).attr("href")).show();
+  event.preventDefault();
 });
 
 
-// ・.on('click', function()は、.menu-triggerがクリックされたときに、処理が行われるように設定。
-// 　　→.menu-triggerは、ハンバーガーアイコン<a class="menu-trigger" href="#">に設定されている。
+// ・$()関数では、特定の属性を持つ要素や属性値を絞り込むことができる。
+//　 　→特定の属性を持つ要素のみ取得したいときは、「$(要素'[属性]')」と記述する。
+// 　　→属性値が特定の値に等しい要素だけを取得したいときは、「$('要素[属性 = "値"])'」と記述する。
+// 　　→属性値が特定の値に等しくない要素だけを取得したいときは、「$('要素[属性 != "値"]')」と記述する。
 
-// ・.toggleClass()は、指定されているclass（ここではactive）をトグル処理するメソッド。
-// 　　→トグル処理では、該当のclass属性がある場合にはclassが削除され、ない場合にはclassが追加される。
-// 　　　→.menu-trigger.active span:nth-child(1)～(3)の各CSSによって、activeクラスになったときにアイコンが「×」になるように設定されている。
-// 　　　→$(this).toggleClass('active');と書くことで、アイコンが切り替わる。
+// ・.hide();は、特定のHTML要素を非表示にするメソッド。
+// 　　→$('#tab-contents .tab[id != "tab1"]').hide();と記述することで、id="tab-contents"の要素のうち、 class="tab"であり、id="tab1"ではない要素が非表示になる。
 
-// ・.fadeToggle()は、要素のフェードイン・フェードアウトを切り替えるメソッド。
-// 　　→ハンバーガーメニューを開いたときの状態を設定。
+/*
+$('#tab-menu a').on('click', function(event) {
+  // イベント発生時に行われる処理
+});
+*/
+// ・「id="tab-menu"のaタグがクリックされたときに、処理を実行してください」の意味になる。
 
-// ・#sp-menuが開いたときに、.fadeToggle()が処理されるように設定。
-// 　　→#sp-menuは、ハンバーガーメニュー<div id="sp-menu">に設定されている。
-// 　　　→ハンバーガーメニューがクリックされたときに、フェードインとフェードアウトが交互に実行されるようになる。
+// $("#tab-contents .tab").hide();
+// ・id="tab-contents"内のいずれかのタブがクリックされたときに、そのタブ内の全コンテンツをいったん非表示にしている。
+
+// $("#tab-menu .active").removeClass("active");
+// ・.removeClass()は、クラス属性が設定されているHTML要素から、クラスを削除するメソッド。
+// 　　→ここでは、activeが削除対象のクラスとして指定されている。
+// ・CSSの#tab-menu .activeの設定では、タブのクラスがactiveのときは背景色が白色／文字色が黒色になるように設定されている。
+// 　　→タブを切り替えるときは、このCSS設定もいったん削除しなければならないため、.removeClass()にactiveクラスを指定している。
+
+// $(this).addClass("active");
+// ・.addClass()は、任意のHTML要素にクラス属性を追加できるメソッド。
+// 　　→ここでは、activeが追加対象のクラスとして指定されている。
+// 　　→どのときにactiveクラスが追加されるかというと、$(this)のとき、つまり、タブがクリックされたとき。
+
+// $($(this).attr("href")).show();
+// ・.attr()は、HTML要素の属性を取得したり設定できるメソッド。
+// 　　→ここでは、href属性を取得するように指定している。
+// ・.show()は要素を表示するメソッドなので、取得したhref属性の中身が表示されることになる。
+// 　　→どのときに表示されるかというと、$(this)のとき、つまり、タブがクリックされたとき。
